aaae9e8fedbdbab25088c8653bb35d5358543183
fan <xiaofanms@outlook.com>
2025-03-13 18:04:41 +0800

    提交代码。

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,24 @@
-use std::{fs::{self, File}, io::{self, read_to_string, Error, Read}, cmp::Ordering};
-use rand::Rng;
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+  let mut largest = &list[0];
+
+  for item in list {
+      if item > largest {
+          largest = item;
+      }
+  }
+
+  largest
+}
 
 
 fn main() {
-  // panic!("I want to panic!");
-  let greeting_file = File::open("hello.txt");
-  let a = match greeting_file {
-    Ok(file) => file,
-    Err(error) => {
-      println!("Problem opening the file: {:?}", error);
-      File::create("hello.txt").unwrap()
-    }
-  };
-  println!("File opened!");
-  read_username_from_file();
-  fs::remove_file("hello.txt");
+  let number_list = vec![34, 50, 25, 100, 65];
 
-  loop {
-    let mut guess = String::new();
-    let secret_number = rand::thread_rng().gen_range(1..=10);
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-    println!("guess: {}", guess);
-    let guess: i32 = match guess.trim().parse() {
-      Ok(num) => num,
-      Err(err) => {
-        println!("{:?}", err);
-        continue;
-      }
-    };
-    if guess < 1 || guess > 10 {
-      println!("数值不对");
-      continue;
-    }
-    match guess.cmp(&secret_number) {
-      Ordering::Less => println!("猜小了"),
-      Ordering::Equal => println!("恭喜你猜中了"),
-      Ordering::Greater => println!("猜大了")
-    };
-  }
-}
+  let result = largest(&number_list);
+  println!("The largest number is {result}");
 
-fn read_username_from_file() -> Result<String, std::io::Error> {
-  let username_file_result = File::open("hello.txt");
-  let mut username_file = match username_file_result {
-    Ok(file) => file,
-    Err(error) => return Err(error),
-  };
-  let mut username_string = String::new();
-  let a = match username_file.read_to_string(&mut username_string) {
-    Ok(_) => Ok(username_string.clone()),
-    Err(error) => Err(error),
-  };
-  let a = match read_to_string(&username_file) {
-    Ok(_) => Ok(username_string.clone()),
-    Err(error) => Err(error)
-  };
-  return a;
+  let char_list = vec!['y', 'm', 'a', 'q'];
+
+  let result = largest(&char_list);
+  println!("The largest char is {result}");
 }
