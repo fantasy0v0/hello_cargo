51ca0daabfffa13889afc9ef98eb6543501dc0a4
fan <xiaofanms@outlook.com>
2025-03-18 15:53:13 +0800

    feat: 添加动物相关功能及ToString trait实现
    添加了`give_animal`函数返回实现了`Animal` trait的对象，并增加了`print_animal_name`系列函数用于打印动物名称。此外，实现了`ToString` trait，允许通过`to_string_fan`方法将对象转换为字符串。

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::{fmt::write, path::Display};
+use std::{fmt::{write, Display}};
+
+use rand::Rng;
 
 fn main() {
   let r = &123;
@@ -28,6 +30,24 @@ fn main() {
   println!("{}", dog);
   let cat = Cat {};
   println!("dog: {}, cat: {}", dog.name(), cat.name());
+  println!("cat: {}", give_animal().name());
+  print_animal_name(&dog);
+  print_animal_name(&cat);
+
+  print_animal_name_1(&dog);
+  print_animal_name_1(&cat);
+
+  print_animal_name_2(&dog);
+  // print_animal_name_2(&cat);
+  
+  3.to_string_fan();
+  dog.to_string_fan();
+
+  let novel = String::from("Call me Ishmael. Some years ago...");
+  let first_sentence = novel.split('.').next().unwrap();
+  let i = ImportantExcerpt {
+      part: first_sentence,
+  };
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
@@ -70,4 +90,43 @@ impl Animal for Cat {
   fn name(&self) -> &str {
     "This is a 🐱"
   }
+}
+
+fn give_animal() -> impl Animal {
+  // let number = rand::thread_rng().gen_range(1..=10);
+  // if (number > 5) {
+  //   return Dog {};
+  // } else {
+  //   return Cat {};
+  // }
+  Cat {}
+}
+
+fn print_animal_name(animal: &impl Animal) {
+  println!("{}", animal.name());
+}
+
+fn print_animal_name_1<T: Animal>(animal: &T) {
+  println!("{}", animal.name());
+}
+
+fn print_animal_name_2<T>(animal: &T)
+where 
+  T: Animal + Display
+{
+  println!("{}", animal.name());
+}
+
+trait ToString {
+  fn to_string_fan(&self) -> String;
+}
+
+impl<T: Display> ToString for T {
+  fn to_string_fan(&self) -> String {
+    format!("{}", self)
+  }
+}
+
+struct ImportantExcerpt<'a> {
+  part: &'a str,
 }
