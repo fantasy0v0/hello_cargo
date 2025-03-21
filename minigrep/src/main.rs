118606ed0d7293b7356a71023874e727cc028344
fan <xiaofanms@outlook.com>
2025-03-21 18:04:15 +0800

    111

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,45 @@
 use std::env;
+use std::fs;
+use std::process;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
-  dbg!(args);
+  let config = Config::build(&args).unwrap_or_else(|err| {
+    println!("{}", err);
+    process::exit(1);
+  });
+  println!("Searching for {}", config.query);
+  println!("In file {}", config.file_path);
+
+  let contents =
+    fs::read_to_string(config.file_path).expect("Should have been able to read to file");
+  println!("file contents: \n{}", contents);
+}
+
+struct Config {
+  query: String,
+  file_path: String,
+}
+
+impl Config {
+  fn new(args: &[String]) -> Config {
+    if env::args().len() < 3 {
+      panic!("Not enough arguments.");
+    }
+    return parse_config(args);
+  }
+
+  fn build(args: &[String]) -> Result<Config, &str> {
+    if env::args().len() < 3 {
+      return Err("Not enough arguments.");
+    }
+    return Ok(parse_config(args));
+  }
+}
+
+fn parse_config(args: &[String]) -> Config {
+  Config {
+    query: args[1].clone(),
+    file_path: args[2].clone(),
+  }
 }
