2e9573ee107d6f13bb0754ea19fc2ee5337555ab
fan <xiaofanms@outlook.com>
2025-07-18 17:58:15 +0800

    提交代码。

--- student_manager/src/main.rs
+++ student_manager/src/main.rs
@@ -1,24 +1,16 @@
-use std::io::{self, Write};
-
 mod student;
 use student::User;
+mod ui;
+use ui::{read_line, write_line};
 
 fn main() {
   println!("Hello, world!");
   let user = User::new("admin", "123456");
-  let mut username = String::new();
-  io::stdout().write("请输入用户名：".as_bytes()).expect("输出失败");
-  io::stdout().flush().expect("flush失败");
-  io::stdin().read_line(&mut username).expect("请输入用户名");
-  let mut password = String::new();
-  io::stdout().write("请输入密码：".as_bytes()).expect("输出失败");
-  io::stdout().flush().expect("flush失败");
-  io::stdin().read_line(&mut password).expect("请输入密码");
-  let username = username.trim();
-  let password = password.trim();
-  if user.matches(username, password) {
-    println!("登录成功");
+  let username = read_line("请输入用户名：").trim().to_string();
+  let password = read_line("请输入密码：").trim().to_string();
+  if user.matches(&username, &password) {
+    write_line("登录成功");
   } else {
-    println!("登录失败");
+    write_line("登录失败");
   }
 }
---
+++ student_manager/src/ui/mod.rs
@@ -0,0 +1,14 @@
+use std::io::{self, Write};
+
+pub fn read_line(prompt: &str) -> String {
+  io::stdout().write(prompt.as_bytes()).expect("输出失败");
+  io::stdout().flush().expect("flush失败");
+  let mut input = String::new();
+  io::stdin().read_line(&mut input).expect("输入失败");
+  input
+}
+
+pub fn write_line(data: &str) {
+  io::stdout().write(data.as_bytes()).expect("输出失败");
+  io::stdout().flush().expect("flush失败");
+}
