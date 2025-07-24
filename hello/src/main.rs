503cdab6e6ba2ec13ff85dd436364fe2b96b5bd0
fan <xiaofanms@outlook.com>
2025-07-24 16:58:10 +0800

    提交修改。

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,18 +1,8 @@
-use std::process::Stdio;
+use std::{process::Stdio, rc::Rc};
 
 
 fn main() {
-  println!("Hello, world!");
-  // 执行系统的ping命令
-  // let result = std::process::Command::new("ping")
-  let mut result = std::process::Command::new("ping")
-    .arg("127.0.0.1")
-    // .stdout(Stdio::piped())
-    .spawn()
-    .expect("ping failed to start");  
-  // let out = result.wait_with_output().expect("ping failed to wait");
-  let status = result.wait().expect("ping failed to wait");  
-  println!("process: {}, code: {} success:{}", result.id(), status.code().unwrap_or_default(), status.success());
-  // println!("输出: {}", String::from_utf8_lossy(&out.stdout));
-  println!("执行结束!");
+  let a = Rc::new(123);
+  let b = Rc::clone(&a);
+  println!("b: {}, rc: {}", b, Rc::strong_count(&a));
 }
