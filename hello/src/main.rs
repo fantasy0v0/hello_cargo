73b72009152620482953e1316e1e11c5725e7889
fan <xiaofanms@outlook.com>
2025-08-11 17:48:04 +0800

    切换到第16章。

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,26 +1,4 @@
-use std::cell::RefCell;
 
 fn main() {
   println!("hello world");
-  let a = RefCell::new(5);
-  *(a.borrow_mut()) = 10;
-  println!("a: {}", a.borrow());
-}
-
-use crate::List::{Cons, Nil};
-use std::rc::Rc;
-
-#[derive(Debug)]
-enum List {
-  Cons(i32, RefCell<Rc<List>>),
-  Nil,
-}
-
-impl List {
-  fn tail(&self) -> Option<&RefCell<Rc<List>>> {
-    match self {
-      Cons(_, item) => Some(item),
-      Nil => None,
-    }
-  }
 }
